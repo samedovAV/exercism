8c4e4be4b6e7fed6e4ed2e534f1123f6cd6c436d
Samedov Aleksandr <alekcandr206@gmail.com>
2023-09-24 14:34:26 +0200

    solved java: annalyns-infiltration

---
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -0,0 +1,23 @@
+class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        return archerIsAwake || knightIsAwake || prisonerIsAwake;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
+        if (petDogIsPresent && !archerIsAwake) {
+            return true;
+        }
+        if (prisonerIsAwake && !knightIsAwake && !archerIsAwake) {
+            return true;
+        }
+        return false;
+    }
+}

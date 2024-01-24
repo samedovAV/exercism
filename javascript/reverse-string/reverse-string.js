69657cb1926d8edde063b617e29dd27b87c99cb9
Aleksandr <alekcandr206@gmail.com>
2024-01-24 22:59:26 +0100

    updated reverse-string in js

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,9 +1,24 @@
-//
-// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+// Built-In Functions
+function reverseStringBuiltIn(str) {
+  return str.split('').reverse().join('');
+}
 
-export const reverseString = (str) => str.split('').reverse().join('');
+// Spread Operator
+function reverseStringSpreadOperator(str) {
+  return [...str].reverse().join('');
+}
 
-/*export const reverseStringPureJS = (str) => {
-}*/
+// For Loop Approach
+function reverseStringForLoop(str) {
+  let reversedStr = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
+}
+
+// Recursion Approach (Ternary)
+function reverseStringRecursionTernary(str) {
+  return (str === '') ? '' : reverseStringRecursionTernary(str.substr(1)) + str.charAt(0);
+}
+

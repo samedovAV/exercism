8a2a4729e7788247b07c0e7c9c4cc35f722a3d2b
Aleksandr Samedov <alekcandr206@gmail.com>
2024-10-10 17:36:37 +0200

    feat(java): pangram

---
+++ java/pangram/src/main/java/PangramChecker.java
@@ -0,0 +1,10 @@
+public class PangramChecker {
+
+    public boolean isPangram(String input) {
+        return input.toLowerCase().chars()
+            .filter(i -> i >= 'a' && i <= 'z')
+            .distinct()
+            .count() == 26;
+    }
+
+}

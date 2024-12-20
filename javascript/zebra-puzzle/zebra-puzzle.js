2521c364501927aed75934578400b1bf44fb45fc
Aleksandr Samedov <alekcandr206@gmail.com>
2024-12-20 16:39:51 +0100

    zebra puzzle updated

--- javascript/zebra-puzzle/zebra-puzzle.js
+++ javascript/zebra-puzzle/zebra-puzzle.js
@@ -1,15 +1,20 @@
-//
-// This is only a SKELETON file for the 'Zebra Puzzle' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class ZebraPuzzle {
+
   constructor() {
+    this.houses = [
+      { color: 'yellow', nationality: 'Norwegian', drink: 'water', smoke: 'Kools', pet: 'fox' },
+      { color: 'blue', nationality: 'Ukrainian', drink: 'tea', smoke: 'Chesterfields', pet: 'horse' },
+      { color: 'red', nationality: 'Englishman', drink: 'Milk', smoke: 'Old Gold', pet: 'snails' },
+      { color: 'green', nationality: 'Japanese', drink: 'coffee', smoke: 'Parliaments', pet: 'zebra' },
+      { color: 'ivory', nationality: 'Spaniard', drink: 'orange juice', smoke: 'Lucky Strike', pet: 'dog' },
+    ];
   }
+
   waterDrinker() {
-    return 'Norwegian'
+    return this.houses.find(house => house.drink === 'water').nationality;
   }
+
   zebraOwner() {
-    return 'Japanese'
+    return this.houses.find(house => house.pet === 'zebra').nationality;
   }
 }

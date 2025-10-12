b076e26b157cabe8b075fa68b3e687e43d6ea2d2
Aleksandr Samedov <alekcandr206@gmail.com>
2025-10-12 14:01:03 +0200

    feat(java): bird-watcher

---
+++ java/bird-watcher/src/main/java/BirdWatcher.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+class BirdWatcher {
+    private final int[] birdsPerDay;
+
+    public BirdWatcher(int[] birdsPerDay) {
+        this.birdsPerDay = birdsPerDay.clone();
+    }
+
+    public int[] getLastWeek() {
+        return birdsPerDay;
+    }
+
+    public int getToday() {
+        return birdsPerDay.length == 0 ? 0 : birdsPerDay[birdsPerDay.length -1];
+    }
+
+    public void incrementTodaysCount() {
+        birdsPerDay[birdsPerDay.length - 1]++;
+    }
+
+    public boolean hasDayWithoutBirds() {
+        return Arrays.stream(birdsPerDay).filter(a -> a == 0).findAny().isPresent();
+    }
+
+    public int getCountForFirstDays(int numberOfDays) {
+        return Arrays.stream(birdsPerDay).limit(numberOfDays).sum();
+    }
+
+    public int getBusyDays() {
+        return (int) Arrays.stream(birdsPerDay).filter(a -> a >= 5).count();
+    }
+}
